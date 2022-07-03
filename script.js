d848bc91ef1260333d346bc99eaf0f2646c5d252
jumahi <mrjasonu@gmail.com>
2022-07-03 11:56:24 +0100

    improves the website design

--- script.js
+++ script.js
@@ -5,48 +5,74 @@ var paper = document.getElementById('paper')
 var scissors = document.getElementById('scissors')
 var random = document.getElementById('random')
 
-function rockPaperScissors(userChoice) {
+function rockPaperScissors(userChoice, chosenByRandom) {
     var choices = ['rock', 'scissors', 'paper']
     var computerChoice = choices[Math.floor(Math.random() * 3)]
-    if (userChoice === computerChoice) {
-        winOrLose.innerHTML = 'DRAW!' + ' Computer chose: ' + computerChoice
-    } else if (userChoice === 'rock') {
-        if (computerChoice === 'paper') {
-            winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice
-        } else if (computerChoice === 'scissors') {
-            winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice
+    if (chosenByRandom === false) {
+        if (userChoice === computerChoice) {
+            winOrLose.innerHTML = 'DRAW!' + ' Computer chose: ' + computerChoice
+        } else if (userChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice
+            } else if (computerChoice === 'scissors') {
+                winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice
+            }
+        } else if (userChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice
+            } else if (computerChoice === 'rock') {
+                winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice
+            }
+        } else if (userChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice
+            } else if (computerChoice === 'paper') {
+                winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice
+            }
         }
-    } else if (userChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-            winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice
-        } else if (computerChoice === 'rock') {
-            winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice
-        }
-    } else if (userChoice === 'scissors') {
-        if (computerChoice === 'rock') {
-            winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice
-        } else if (computerChoice === 'paper') {
-            winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice
+    } else if (chosenByRandom != false) {
+        if (userChoice === computerChoice) {
+            winOrLose.innerHTML = 'DRAW!' + ' Computer chose: ' + computerChoice + ' Random chose: ' + userChoice
+        } else if (userChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice + ' Random chose: ' + userChoice
+            } else if (computerChoice === 'scissors') {
+                winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice + ' Random chose: ' + userChoice
+            }
+        } else if (userChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice + ' Random chose: ' + userChoice
+            } else if (computerChoice === 'rock') {
+                winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice + ' Random chose: ' + userChoice
+            }
+        } else if (userChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                winOrLose.innerHTML = 'You lost!' + ' Computer chose: ' + computerChoice + ' Random chose: ' + userChoice
+            } else if (computerChoice === 'paper') {
+                winOrLose.innerHTML = 'You win!' + ' Computer chose: ' + computerChoice + ' Random chose: ' + userChoice
+            }
         }
     }
 }
 
 function rockClicked() {
-    rockPaperScissors('rock')
+    rockPaperScissors('rock', false)
 }
 
 function scissorsClicked() {
-    rockPaperScissors('scissors')
+    rockPaperScissors('scissors', false)
 }
 
 function paperClicked() {
-    rockPaperScissors('paper')
+    rockPaperScissors('paper', false)
 }
 
 function randomClicked() {
     var choices = ['rock', 'scissors', 'paper']
     var userChoice = choices[Math.floor(Math.random() * 3)]
+    chosenByRandom = true
     rockPaperScissors(userChoice)
+
 }
 
 rock.onclick = rockClicked
